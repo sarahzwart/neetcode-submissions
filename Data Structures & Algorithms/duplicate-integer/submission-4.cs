dbc2a7d58a930e7161fe8d3459e986581e6b8ce2
Sarah Zwart <128381138+sarahzwart@users.noreply.github.com>
2026-04-15 12:54:31 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        HashSet<int> vals = new HashSet<int>();
+        foreach (int n in nums){
+            if(vals.Contains(n)){
+                return true;
+            }
+            vals.Add(n);
+        }
+        return false;
+    }
+}
