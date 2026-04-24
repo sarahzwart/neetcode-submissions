9425f270412695d428598a39ad4813e2d82c21a9
Sarah Zwart <128381138+sarahzwart@users.noreply.github.com>
2026-04-24 12:53:06 -0400

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        Array.Sort(nums);
+        var res = new List<List<int>>();
+        for(int i = 0; i < nums.Length - 2; i++){
+            // no duplicates
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.Length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.Add(new List<int> { nums[i], nums[left], nums[right] });
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+}
