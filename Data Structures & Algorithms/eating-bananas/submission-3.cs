973d48bb9917f5c20a689c1d0becfe9fe36d17b3
Sarah Zwart <128381138+sarahzwart@users.noreply.github.com>
2026-04-30 10:22:04 -0400

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = piles.Max();
+        int ret = 0;
+
+        while(l <= r){
+            int k = l + (r - l) / 2;
+
+            long totalT = 0;
+            foreach (int p in piles) {
+                totalT += (int)Math.Ceiling((double)p/k);
+            }
+            if(totalT > h){
+                l = k + 1;
+            } else {
+                r = k - 1;
+                ret = k;
+            }
+        }
+        return ret;
+    }
+}
